c00fbc1f2ef8302566039384dc0f497f9018bda0
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2020-01-25 14:50:58 -0300

    169. Connecting to MongoDB

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Setup Express API
 5. mkdir src
 6. Create index.js
 7. node src/index.js
+
+MongoDB Setup
+1. Go to www.cloud.mongodb.com
+2. Create a cluster, user and then get the uri connection string
--- tracker-server/src/index.js
+++ tracker-server/src/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = 'mongodb+srv://admin:admin@cluster0-0tf2v.mongodb.net/test?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUri, {
+   useNewUrlParser: true,
+   useCreateIndex: true 
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo instance', err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hi There!');
