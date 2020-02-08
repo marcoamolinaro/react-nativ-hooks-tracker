abc3ff6347a0b5cf273807dbe240565c1bb00a37
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2020-02-08 09:54:07 -0300

    193. Testing the Navigation Flow

--- tracks/src/screens/AccountScreen.js
+++ tracks/src/screens/AccountScreen.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
-const SigninScreen = () => {
+const Accountcreen = () => {
     return (
         <Text style={{ fontSize: 48 }}>
-            SigninScreen
+            Accountcreen
         </Text>
     );
 };
 
 const styles = StyleSheet.create({});
 
-export default SigninScreen;
+export default Accountcreen;
--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const TrackListScreen = () => {
+const TrackListScreen = ({navigation}) => {
     return (
-        <Text style={{ fontSize: 48 }}>
-            TrackListScreen
-        </Text>
+        <>
+            <Text style={{ fontSize: 48 }}>
+                TrackListScreen
+            </Text>
+            <Button
+                title="Go to TrackDetail"
+                onPress={() => navigation.navigate('TrackDetail')}
+            />
+        </>
     );
 };
 
