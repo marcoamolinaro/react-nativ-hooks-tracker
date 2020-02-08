b6c8cec86a2acd3c90b41c2828b9de9f2eb661d8
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2020-02-08 11:07:44 -0300

    196. Styling Odds and Ends

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -3,22 +3,37 @@ import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
-const AccountScreen = ({navigation}) => {
+const SignupScreen = ({navigation}) => {
     return (
-        <>
+        <View style={styles.container}> 
             <Spacer>
                 <Text h3>Sign Up for Tracks</Text>
             </Spacer>
-            <Input label="EMail"/>
+            <Input label="EMail"/>  
             <Spacer />
             <Input label="Password"/>
             <Spacer>
                 <Button title="Sign Up"/>
             </Spacer>
-        </>
+        </View>
     );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+    return {
+        header: null
+    };
+};
 
-export default AccountScreen;
+const styles = StyleSheet.create({
+    container: {
+        borderColor: 'red',
+        borderWidth: 10,
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 250,
+        marginTop: 30
+    }
+});
+
+export default SignupScreen;
