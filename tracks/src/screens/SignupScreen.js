225cfcc9183ef01a3085a155698a78ccd0deebbd
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2020-02-09 14:46:10 -0300

    197. Input Props

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [ password, setPassword ] = useState('');
+
     return (
         <View style={styles.container}> 
             <Spacer>
                 <Text h3>Sign Up for Tracks</Text>
             </Spacer>
-            <Input label="EMail"/>  
+            <Input 
+                label="EMail"
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize="none"
+                autoCorrect={false}
+            />  
             <Spacer />
-            <Input label="Password"/>
+            <Input 
+                label="Password"
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry
+            />
             <Spacer>
                 <Button title="Sign Up"/>
             </Spacer>
@@ -28,7 +44,7 @@ SignupScreen.navigationOptions = () => {
 const styles = StyleSheet.create({
     container: {
         borderColor: 'red',
-        borderWidth: 10,
+        borderWidth: 5,
         flex: 1,
         justifyContent: 'center',
         marginBottom: 250,
