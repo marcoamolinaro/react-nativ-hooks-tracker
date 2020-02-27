ce39697103b3f04a1adeebb95c5725bffab983a5
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2020-02-27 11:41:39 -0300

    243. Cleaning Up After Ourselves

--- tracks/src/hooks/useLocation.js
+++ tracks/src/hooks/useLocation.js
@@ -28,6 +28,13 @@ export default (shouldTrack, callback) => {
            subscriber.remove();
            setSubscriber(null);
         }
+
+        return () => {
+            if (subscriber) {
+                subscriber.remove(); 
+            }
+        };
+
     }, [shouldTrack, callback]);
 
     return [err];
