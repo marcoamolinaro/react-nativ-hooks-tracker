aa3aca36b57082cbf62e057ecfbe14aa4e917aa0
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2020-02-28 09:35:27 -0300

    253. Navigating to a Saved Track

--- tracks/src/screens/TrackDetailScreen.js
+++ tracks/src/screens/TrackDetailScreen.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React,{ useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Context as TrackContext } from '../context/TrackContext';
 
-const TrackDetailScreen = () => {
+const TrackDetailScreen = ({navigation}) => {
+    const { state } = useContext(TrackContext);
+    const _id = navigation.getParam('_id');
+
+    console.log(state);
+
+    const track = state.find(t => t._id === _id);
+
     return (
-        <Text style={{ fontSize: 48 }}>
-            TrackDetailScreen
+        <Text style={{ fontSize: 38 }}>
+            {track.name}
         </Text>
     );
 };
--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -21,7 +21,10 @@ const TrackListScreen = ({navigation}) => {
                 keyExtractor={item => item._id}
                 renderItem={({item}) => {
                     return (
-                        <TouchableOpacity>
+                        <TouchableOpacity 
+                            onPress={() => {
+                                navigation.navigate('TrackDetail', { id: item._id })
+                            }}>
                             <ListItem chevron title={item.name} />
                         </TouchableOpacity>
                     )
